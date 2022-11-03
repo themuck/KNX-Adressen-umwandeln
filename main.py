93f55ec87f74a1acf05f4da94b173e4fdaf167e0
themuck <malte_knaust@gmx.com>
2022-11-03 16:25:28 +0100

    Update main.py
    + adressen plausibel

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 eingabe = ''
 
+print('KNX Gruppenadressen Konverter von the_muck V0.01 \n')
+
 while eingabe != 'quit':
 
-    eingabe = input('KNX Adresse eingeben: \n')
+    eingabe = input('KNX Adresse eingeben: ')
 
     if eingabe.count('/') == 1:
-        print('Format H/U erkannt, konvertiere zu H/M/U')
+        print('Format H/U erkannt, konvertiere zu H/M/U.')
         array = eingabe.split('/')
-        print(array[0]+ '/' + str(int(array[1])//256) + '/' + str(int(array[1])%256)+'\n')
+        if int(array[0]) > 31 or int(array[1]) > 2047:
+            print('Falscher Adressbereich!' + '\n')
+        print('-> ' +array[0]+ '/' + str(int(array[1])//256) + '/' + str(int(array[1])%256)+'\n')
 
     elif eingabe.count('/') == 2:
-        print('Format H/M/U erkannt, konvertiere zu H/U')
+        print('Format H/M/U erkannt, konvertiere zu H/U.')
         array = eingabe.split('/')
-        print(array[0] + '/' + str(int(array[1]) * 256 + int(array[2])) +'\n')
+        if int(array[0]) > 31 or int(array[1]) > 7 or int(array[2]) > 255:
+            print('Falscher Adressbereich!' + '\n')
+        else:
+            print('-> ' + array[0] + '/' + str(int(array[1]) * 256 + int(array[2])) +'\n')
 
     elif eingabe != 'quit':
-        print('Falsches Format, H/M/U oder H/U'+'\n')
+        print('Falsches Format, H/M/U oder H/U!'+'\n')
 
 quit()
